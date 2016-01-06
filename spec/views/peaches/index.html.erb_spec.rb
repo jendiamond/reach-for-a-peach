9018d3b784b96fa0a9fb374d333666a4c423fe36
jendiamond <thejendiamond@gmail.com>
2016-01-06 23:40:40 +0000

    Add peaches/index view

--- spec/views/peaches/index.html.erb_spec.rb
+++ spec/views/peaches/index.html.erb_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "peaches/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe 'peaches', type: :view do
+#   it 'Should return a list of peaches/goals' do
+#     render :template => "peaches/index.html.erb"
+#     rendered.should contain("Peaches")
+#     # expect(page).to have_content('Peaches')
+#   end
+# end
+
+	
+feature 'User creates a foobar' do
+  scenario 'they see the foobar on the page' do
+    visit peaches_path
+    expect(page).to have_css 'h1#peach_title', 'Peaches'
+  end
 end
