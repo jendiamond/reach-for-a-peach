0312aa06cdc761a0f1b69c6e038702bc97c36b24
Kirk <kkchu791@gmail.com>
2016-01-14 17:59:39 -0800

    issue#19 add footer
    Add simple footer to be improve upon

--- spec/views/peaches/index.html.erb_spec.rb
+++ spec/views/peaches/index.html.erb_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
 
-# RSpec.describe 'peaches', type: :view do
-#   it 'Should return a list of peaches/goals' do
-#     render :template => "peaches/index.html.erb"
-#     rendered.should contain("Peaches")
-#     # expect(page).to have_content('Peaches')
-#   end
-# end
-
-feature 'User sees a list of peaches' do
-  scenario 'they see the peaches on the page' do
+feature 'User visits the index page' do
+  scenario 'they see the list of peaches on the page' do
     visit peaches_path
     expect(page).to have_css('h1#peach_title')
+  end
+
+  scenario 'they see the footer' do
+    visit peaches_path
+    expect(page).to have_content('LA Hobby Dev Shop')
   end
 end
